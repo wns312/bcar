c8f4a29f181b63d279d7efdc6f540996324381b3
wns312 <wns312@gmail.com>
2023-09-07 16:09:58 +0900

    [fix] Delete removed services from index.ts

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,5 @@
-export * from "./AccountResetService"
 export * from "./CarAssignService"
 export * from "./CarCollectService"
 export * from "./CarUploadService"
 export * from "./CategoryService"
-export * from "./UploadedCarRemoveService"
 export * from "./UploadedCarSyncService"
